783ca35d0fe41890365fab2ef0fd3ba9a526c5e4
Tanais <tanais@chooseme.ru>
2022-03-01 14:44:30 +0300

    +Add hm5

--- src/main/java/ru/as/java_lvl_3/home_work_5/Tunnel.java
+++ src/main/java/ru/as/java_lvl_3/home_work_5/Tunnel.java
@@ -1,6 +1,13 @@
 package ru.as.java_lvl_3.home_work_5;
 
+import java.util.concurrent.Semaphore;
+
+import static ru.as.java_lvl_3.home_work_5.MainClass.CARS_COUNT;
+
 public class Tunnel extends Stage {
+
+    private Semaphore smp = new Semaphore(CARS_COUNT / 2);
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -10,12 +17,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
